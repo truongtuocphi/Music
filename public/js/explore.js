2a8df3240e8b6c767760c22a007a964aeb4172dd
unknown <truongphi754@gmail.com>
2022-11-28 19:53:55 +0700

    Update: show Explore

--- public/js/explore.js
+++ public/js/explore.js
@@ -1,14 +1,13 @@
-let pageExplore = document.querySelector("#box-explore");
 let showExplore = document.querySelector('#explore');
-let findSong = document.querySelector("#find-sond");
+let findSong = document.querySelector("#find-song");
 
 showExplore.addEventListener('click', function() {
+    let pageExplore = document.querySelector("#box-explore");
     pageExplore.classList.remove('hidden');
 });
 
 findSong.addEventListener('click', function() {
-    let contentExplore = document.querySelector('#content-page-explore');
-
-    contentExplore.classList()
+    let contentExplore = document.querySelector('#in-content-explore');
+    contentExplore.classList.add('hidden');
 })
 
